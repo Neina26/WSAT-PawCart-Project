27d956ec34dc47eec413baf69b5628e3b14fc762
zephyrstorm123 <107976319+zephyrstorm123@users.noreply.github.com>
2024-11-21 09:34:44 +0800

    Updated Pages

--- script/price.js
+++ script/price.js
@@ -1,21 +1,21 @@
 document.querySelectorAll('.form-control').forEach(select => {
     select.addEventListener('change', function() {
         const item = this.closest('.list-group-item');
-        const basePrice = parseFloat(this.options[this.selectedIndex].getAttribute('data-price'));
+        const basePrice = parseInt(document.getElementById('#sum-price').textContent).replace('P', '');
         const quantity = parseInt(this.value);
+        console.log(basePrice, quantity);
         
         if (!isNaN(basePrice) && !isNaN(quantity)) {
             const total = basePrice * quantity;
-            item.querySelector('#sum-price').textContent = 'P' + total.toFixed(2);
+            return updateTotal(total);
         } else {
             item.querySelector('#sum-price').textContent = 'P0';
         }
-        updateTotal();
+
     });
 });
 
-function updateTotal() {
-    let total = 0;
+function updateTotal(total) {
     document.querySelectorAll('.list-group-item').forEach(item => {
         const sumPriceText = item.querySelector('#sum-price')?.textContent;
         if (sumPriceText) {
